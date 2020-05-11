a5c688413d2fea9ac37e758c7285bfa438a7cff8
Rok Povsic <rok.povsic@clarifai.com>
2020-05-11 11:21:03 +0200

    Restructure index.js a bit

--- src/index.js
+++ src/index.js
@@ -21,18 +21,21 @@ const packageDefinition = protoLoader.loadSync(
     }
 );
 
+// This is the JavaScript's best way of cloning an object.
+const jsonPackageDefinition = JSON.parse(JSON.stringify(packageDefinition));
+
 /*
- * When sending the data over the JSON channel, if a concept doesn't have a value, the server defaults the value to 1.
- * With the gRPC channel however, all missing fields get a default value of 0. To do the same in JSON channel what
- * is done in the default gRPC channel, explicit 0 is set.
+ * When sending the data over the JSON channel, if a concept doesn't have a value, the server defaults the value
+ * to 1. With the gRPC channel however, all missing fields get a default value of 0. To do the same in JSON channel
+ * what is done in the default gRPC channel, explicit 0 is set.
  *
  * Ideally this would be done dynamically by inspecting the "cl_default_float" custom option of the proto field, but
  * I'm not sure how to find out whether a field has this set or not.
  */
-function recursivelyAddConceptValue(obj) {
+function _recursivelyAddConceptValue(obj) {
     if (obj.constructor === Array) {
         for (const elem of obj) {
-            recursivelyAddConceptValue(elem);
+            _recursivelyAddConceptValue(elem);
         }
     } else if (obj.constructor === Object) {
         for (const key of Object.keys(obj)) {
@@ -48,37 +51,33 @@ function recursivelyAddConceptValue(obj) {
                     concept["value"] = 0;
                 }
             } else if (key === "base64") {
-                // Skip base64 so we don't unnecessarily iterate over its data array.
+                // Skip base64 so we don't unnecessarily iterate over its long data array.
             } else {
-                recursivelyAddConceptValue(obj[key]);
+                _recursivelyAddConceptValue(obj[key]);
             }
         }
     }
 }
 
-const grpcProtoDescriptor = grpc.loadPackageDefinition(packageDefinition);
-
-// This is the JavaScript's best way of cloning an object.
-const jsonPackageDefinition = JSON.parse(JSON.stringify(packageDefinition));
-const jsonService = jsonPackageDefinition["clarifai.api.V2"];
-for (const methodName in jsonService) {
-    if (jsonService.hasOwnProperty(methodName)) {
-        jsonService[methodName].requestSerialize = (request) => {
-            recursivelyAddConceptValue(request);
-            return request;
-        }
-        jsonService[methodName].responseDeserialize = (data) => data
-    }
-}
-const jsonProtoDescriptor = grpc.loadPackageDefinition(jsonPackageDefinition);
-
-
 class ClarifaiStub {
     static insecureGrpc() {
+        const grpcProtoDescriptor = grpc.loadPackageDefinition(packageDefinition);
         return new grpcProtoDescriptor.clarifai.api.V2('api-grpc.clarifai.com:18080', grpc.credentials.createInsecure());
     }
 
     static json() {
+        const jsonService = jsonPackageDefinition["clarifai.api.V2"];
+        for (const methodName in jsonService) {
+            if (jsonService.hasOwnProperty(methodName)) {
+                jsonService[methodName].requestSerialize = (request) => {
+                    _recursivelyAddConceptValue(request);
+                    return request;
+                }
+                jsonService[methodName].responseDeserialize = (data) => data
+            }
+        }
+
+        const jsonProtoDescriptor = grpc.loadPackageDefinition(jsonPackageDefinition);
         return new jsonProtoDescriptor.clarifai.api.V2('', null, {channelOverride: ClarifaiChannel.makeJsonChannel()});
     }
 }
