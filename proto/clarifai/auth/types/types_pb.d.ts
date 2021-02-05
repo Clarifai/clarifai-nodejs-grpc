5d8d86faf43c47c72cca861efdb1a8f743bbabdb
Rok Povsic <rok.povsic@clarifai.com>
2021-02-05 18:18:19 +0100

    Version 7.2.0

---
+++ proto/clarifai/auth/types/types_pb.d.ts
@@ -0,0 +1,16 @@
+// package: clarifai.auth.types
+// file: proto/clarifai/auth/types/types.proto
+
+/* tslint:disable */
+/* eslint-disable */
+
+import * as jspb from "google-protobuf";
+
+export enum AuthType {
+    UNDEF = 0,
+    NOAUTH = 1,
+    KEYAUTH = 2,
+    SESSIONTOKENAUTH = 3,
+    ADMINAUTH = 4,
+    PATAUTH = 5,
+}
