663167c7caaee655883967345570d93342b1f5e4
clarifai-prod <clarifai-prod@users.noreply.github.com>
2025-04-11 00:25:42 +0000

    GRPC clients version 11.3.2

--- proto/clarifai/api/resources_pb.d.ts
+++ proto/clarifai/api/resources_pb.d.ts
@@ -2696,6 +2696,11 @@ export class Model extends jspb.Message {
     getBillingType(): Model.BillingType;
     setBillingType(value: Model.BillingType): Model;
 
+    hasFeaturedOrder(): boolean;
+    clearFeaturedOrder(): void;
+    getFeaturedOrder(): google_protobuf_wrappers_pb.Int32Value | undefined;
+    setFeaturedOrder(value?: google_protobuf_wrappers_pb.Int32Value): Model;
+
     serializeBinary(): Uint8Array;
     toObject(includeInstance?: boolean): Model.AsObject;
     static toObject(includeInstance: boolean, msg: Model): Model.AsObject;
@@ -2740,6 +2745,7 @@ export namespace Model {
         creator: string,
         versionCount: number,
         billingType: Model.BillingType,
+        featuredOrder?: google_protobuf_wrappers_pb.Int32Value.AsObject,
     }
 
     export enum Source {
